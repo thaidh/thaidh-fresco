5115848542ae13f04bd342afd3ee02ce44ad3782
thaidh <thaidh@vng.com.vn>
2016-10-17 18:07:30 +0700

    - add essential lib

---
+++ drawee-backends/drawee-pipeline/src/main/java/com/facebook/drawee/backends/pipeline/PipelineDraweeControllerFactory.java
@@ -0,0 +1,80 @@
+/*
+ * Copyright (c) 2015-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ */
+
+package com.facebook.drawee.backends.pipeline;
+
+import java.util.concurrent.Executor;
+
+import android.content.res.Resources;
+
+import com.facebook.cache.common.CacheKey;
+import com.facebook.common.internal.ImmutableList;
+import com.facebook.common.internal.Supplier;
+import com.facebook.common.references.CloseableReference;
+import com.facebook.datasource.DataSource;
+import com.facebook.drawee.components.DeferredReleaser;
+import com.facebook.imagepipeline.cache.MemoryCache;
+import com.facebook.imagepipeline.image.CloseableImage;
+import com.facebook.imagepipeline.animated.factory.AnimatedDrawableFactory;
+
+import javax.annotation.Nullable;
+
+/**
+ * Default implementation of {@link PipelineDraweeControllerFactory}.
+ */
+public class PipelineDraweeControllerFactory {
+
+  private Resources mResources;
+  private DeferredReleaser mDeferredReleaser;
+  private AnimatedDrawableFactory mAnimatedDrawableFactory;
+  private Executor mUiThreadExecutor;
+  private MemoryCache<CacheKey, CloseableImage> mMemoryCache;
+  @Nullable
+  private ImmutableList<DrawableFactory> mDrawableFactories;
+  private boolean mDrawDebugOverlay;
+
+  public PipelineDraweeControllerFactory(
+      Resources resources,
+      DeferredReleaser deferredReleaser,
+      AnimatedDrawableFactory animatedDrawableFactory,
+      Executor uiThreadExecutor,
+      MemoryCache<CacheKey, CloseableImage> memoryCache,
+      @Nullable ImmutableList<DrawableFactory> drawableFactories,
+      boolean drawDebugOverlay) {
+    mResources = resources;
+    mDeferredReleaser = deferredReleaser;
+    mAnimatedDrawableFactory = animatedDrawableFactory;
+    mUiThreadExecutor = uiThreadExecutor;
+    mMemoryCache = memoryCache;
+    mDrawableFactories = drawableFactories;
+    mDrawDebugOverlay = drawDebugOverlay;
+  }
+
+  public PipelineDraweeController newController(
+      Supplier<DataSource<CloseableReference<CloseableImage>>> dataSourceSupplier,
+      String id,
+      CacheKey cacheKey,
+      Object callerContext) {
+    PipelineDraweeController controller = new PipelineDraweeController(
+        mResources,
+        mDeferredReleaser,
+        mAnimatedDrawableFactory,
+        mUiThreadExecutor,
+        mMemoryCache,
+        dataSourceSupplier,
+        id,
+        cacheKey,
+        callerContext,
+        mDrawableFactories);
+    if (mDrawDebugOverlay) {
+      controller.setDrawDebugOverlay(true);
+    }
+    return controller;
+  }
+}
---
+++ imagepipeline/src/main/java/com/facebook/imagepipeline/core/ImagePipelineExperiments.java
@@ -0,0 +1,207 @@
+/*
+ * Copyright (c) 2015-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ */
+package com.facebook.imagepipeline.core;
+
+import com.facebook.common.webp.WebpBitmapFactory;
+import com.facebook.imagepipeline.producers.WebpTranscodeProducer;
+
+/**
+ * Encapsulates additional elements of the {@link ImagePipelineConfig} which are currently in an
+ * experimental state.
+ *
+ * <p>These options may often change or disappear altogether and it is not recommended to change
+ * their values from their defaults.
+ */
+public class ImagePipelineExperiments {
+
+  private final int mForceSmallCacheThresholdBytes;
+  private final boolean mWebpSupportEnabled;
+  private final @WebpTranscodeProducer.EnhancedTranscodingType int mEnhancedWebpTranscodingType;
+  private final boolean mDecodeFileDescriptorEnabled;
+  private final int mThrottlingMaxSimultaneousRequests;
+  private final boolean mExternalCreatedBitmapLogEnabled;
+  private final boolean mMediaVariationsEnabled;
+  private final WebpBitmapFactory.WebpErrorLogger mWebpErrorLogger;
+  private final boolean mDecodeCancellationEnabled;
+  private final WebpBitmapFactory mWebpBitmapFactory;
+  private final boolean mSuppressBitmapPrefetching;
+
+  private ImagePipelineExperiments(Builder builder, ImagePipelineConfig.Builder configBuilder) {
+    mForceSmallCacheThresholdBytes = builder.mForceSmallCacheThresholdBytes;
+    mWebpSupportEnabled = builder.mWebpSupportEnabled;
+    mEnhancedWebpTranscodingType = builder.mEnhancedWebpTranscodingType;
+    mDecodeFileDescriptorEnabled = configBuilder.isDownsampleEnabled() &&
+        builder.mDecodeFileDescriptorEnabled;
+    mThrottlingMaxSimultaneousRequests = builder.mThrottlingMaxSimultaneousRequests;
+    mExternalCreatedBitmapLogEnabled = builder.mExternalCreatedBitmapLogEnabled;
+    mMediaVariationsEnabled = builder.mMediaVariationsEnabled;
+    mWebpErrorLogger = builder.mWebpErrorLogger;
+    mDecodeCancellationEnabled = builder.mDecodeCancellationEnabled;
+    mWebpBitmapFactory = builder.mWebpBitmapFactory;
+    mSuppressBitmapPrefetching = builder.mSuppressBitmapPrefetching;
+  }
+
+  public boolean isDecodeFileDescriptorEnabled() {
+    return mDecodeFileDescriptorEnabled;
+  }
+
+  public boolean isExternalCreatedBitmapLogEnabled() {
+    return mExternalCreatedBitmapLogEnabled;
+  }
+
+  public int getForceSmallCacheThresholdBytes() {
+    return mForceSmallCacheThresholdBytes;
+  }
+
+  public boolean isMediaVariationsEnabled() {
+    return mMediaVariationsEnabled;
+  }
+
+  public boolean isWebpSupportEnabled() {
+    return mWebpSupportEnabled;
+  }
+
+  public boolean isDecodeCancellationEnabled() {
+    return mDecodeCancellationEnabled;
+  }
+
+  public @WebpTranscodeProducer.EnhancedTranscodingType int getEnhancedWebpTranscodingType() {
+    return mEnhancedWebpTranscodingType;
+  }
+
+  public int getThrottlingMaxSimultaneousRequests() {
+    return mThrottlingMaxSimultaneousRequests;
+  }
+
+  public WebpBitmapFactory.WebpErrorLogger getWebpErrorLogger() {
+    return mWebpErrorLogger;
+  }
+
+  public WebpBitmapFactory getWebpBitmapFactory() {
+    return mWebpBitmapFactory;
+  }
+
+  public static ImagePipelineExperiments.Builder newBuilder(
+      ImagePipelineConfig.Builder configBuilder) {
+    return new ImagePipelineExperiments.Builder(configBuilder);
+  }
+
+  public static class Builder {
+
+    private static final int DEFAULT_MAX_SIMULTANEOUS_FILE_FETCH_AND_RESIZE = 5;
+
+    private final ImagePipelineConfig.Builder mConfigBuilder;
+    private int mForceSmallCacheThresholdBytes = 0;
+    private boolean mWebpSupportEnabled = false;
+    private @WebpTranscodeProducer.EnhancedTranscodingType int mEnhancedWebpTranscodingType;
+    private boolean mDecodeFileDescriptorEnabled = false;
+    private boolean mExternalCreatedBitmapLogEnabled = false;
+    private int mThrottlingMaxSimultaneousRequests = DEFAULT_MAX_SIMULTANEOUS_FILE_FETCH_AND_RESIZE;
+    private boolean mMediaVariationsEnabled = false;
+    private WebpBitmapFactory.WebpErrorLogger mWebpErrorLogger;
+    private boolean mDecodeCancellationEnabled = false;
+    private WebpBitmapFactory mWebpBitmapFactory;
+    private boolean mSuppressBitmapPrefetching = false;
+
+    public Builder(ImagePipelineConfig.Builder configBuilder) {
+      mConfigBuilder = configBuilder;
+    }
+
+    public ImagePipelineConfig.Builder setDecodeFileDescriptorEnabled(
+        boolean decodeFileDescriptorEnabled) {
+      mDecodeFileDescriptorEnabled = decodeFileDescriptorEnabled;
+      return mConfigBuilder;
+    }
+
+    public ImagePipelineConfig.Builder setExternalCreatedBitmapLogEnabled(
+        boolean externalCreatedBitmapLogEnabled) {
+      mExternalCreatedBitmapLogEnabled = externalCreatedBitmapLogEnabled;
+      return mConfigBuilder;
+    }
+
+    /**
+     * If this value is nonnegative, then all network-downloaded images below this size will be
+     * written to the small image cache.
+     *
+     * <p>This will require the image pipeline to do up to two disk reads, instead of one, before
+     * going out to network. Use only if this pattern makes sense for your application.
+     */
+    public ImagePipelineConfig.Builder setForceSmallCacheThresholdBytes(
+        int forceSmallCacheThresholdBytes) {
+      mForceSmallCacheThresholdBytes = forceSmallCacheThresholdBytes;
+      return mConfigBuilder;
+    }
+
+    /**
+     * If true, this will allow the image pipeline to look at each request's
+     * {@link com.facebook.imagepipeline.request.MediaVariations} object to possibly provide
+     * fallback images which are present in cache.
+     */
+    public ImagePipelineConfig.Builder setMediaVariationsEnabled(boolean mediaVariationsEnabled) {
+      mMediaVariationsEnabled = mediaVariationsEnabled;
+      return mConfigBuilder;
+    }
+
+    public ImagePipelineConfig.Builder setWebpSupportEnabled(boolean webpSupportEnabled) {
+      mWebpSupportEnabled = webpSupportEnabled;
+      return mConfigBuilder;
+    }
+
+    /**
+     * If true we cancel decoding jobs when the related request has been cancelled
+     * @param decodeCancellationEnabled If true the decoding of cancelled requests are cancelled
+     * @return The Builder itself for chaining
+     */
+    public ImagePipelineConfig.Builder setDecodeCancellationEnabled(
+        boolean decodeCancellationEnabled) {
+      mDecodeCancellationEnabled = decodeCancellationEnabled;
+      return mConfigBuilder;
+    }
+
+    public ImagePipelineConfig.Builder setWebpErrorLogger(
+        WebpBitmapFactory.WebpErrorLogger webpErrorLogger) {
+      mWebpErrorLogger = webpErrorLogger;
+      return mConfigBuilder;
+    }
+
+    public ImagePipelineConfig.Builder setWebpBitmapFactory(
+        WebpBitmapFactory webpBitmapFactory) {
+      mWebpBitmapFactory = webpBitmapFactory;
+      return mConfigBuilder;
+    }
+
+    public ImagePipelineConfig.Builder setEnhancedWebpTranscodingType(
+        @WebpTranscodeProducer.EnhancedTranscodingType int enhancedWebpTranscodingType) {
+      mEnhancedWebpTranscodingType = enhancedWebpTranscodingType;
+      return mConfigBuilder;
+    }
+
+    public ImagePipelineConfig.Builder setSuppressBitmapPrefetching(
+        boolean suppressBitmapPrefetching) {
+      mSuppressBitmapPrefetching = suppressBitmapPrefetching;
+      return mConfigBuilder;
+    }
+
+    /**
+     * Using this method is possible to change the max number of threads for loading and sizing
+     * local images
+     * @param throttlingMaxSimultaneousRequests Max number of thread
+     * @return The Builder itself for chaining
+     */
+    public ImagePipelineConfig.Builder setThrottlingMaxSimultaneousRequests(
+        int throttlingMaxSimultaneousRequests) {
+      mThrottlingMaxSimultaneousRequests = throttlingMaxSimultaneousRequests;
+      return mConfigBuilder;
+    }
+
+    public ImagePipelineExperiments build() {
+      return new ImagePipelineExperiments(this, mConfigBuilder);
+    }
+  }
+}
